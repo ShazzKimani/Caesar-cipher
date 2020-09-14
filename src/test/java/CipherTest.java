66e9cc8f21a1ea3361d5cc46edfa566bc84274af
Sharon Kimani <sharonkimani01@gmail.com>
2020-09-14 15:59:20 +0300

    Added logic to CipherTest file

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,5 +1,20 @@
-import static org.junit.Assert.*;
+import org.junit.*;
+import static  org.junit.Assert.*;
 
 public class CipherTest {
-
+    @Test
+    public void theText_shouldBeTheText_hello(){
+        Cipher textInput = new Cipher("hello",2);
+        assertEquals("hello" ,textInput.getText());
+    }
+    @Test
+    public void theKey_shouldBeTheKey_2(){
+        Cipher keyInput = new Cipher("hello",2);
+        assertEquals(2,keyInput.getKey());
+    }
+    @Test
+    public void textEncryption_encryptTextBy2_encryptedText(){
+        Cipher textInput = new Cipher("hello",2);
+        assertEquals("jgnnq",textInput.encryptText());
+    }
 }
