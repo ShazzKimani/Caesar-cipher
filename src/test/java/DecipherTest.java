90994ca0c34d1b212529c3b236db6e14c4b7b5f3
Sharon Kimani <sharonkimani01@gmail.com>
2020-09-16 11:03:16 +0300

    Made changes in decipher

--- src/test/java/DecipherTest.java
+++ src/test/java/DecipherTest.java
@@ -1,5 +1,22 @@
-import static org.junit.Assert.*;
+import org.junit.*;
+import static  org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 
 public class DecipherTest {
-
+    @Test
+    public void theText_shouldBeTheText_textEntered(){
+        Decipher textInput = new Decipher("jgnnq",2);
+        assertEquals("jgnnq" ,textInput.getText());
+    }
+    @Test
+    public void theKey_shouldBeTheKey_keyEntered(){
+        Decipher keyInput = new Decipher("jgnnq",2);
+        assertEquals(2,keyInput.getKey());
+    }
+    @Test
+    public void textDecryption_decryptTextBy2_decryptedText(){
+        Decipher textInput = new Decipher("jgnnq",2);
+        assertEquals("hello",textInput.decryptText());
+    }
 }
